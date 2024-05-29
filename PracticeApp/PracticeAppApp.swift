5f8a7c154653294202761f1fb4fbd2e63052c8d0
ideareverse <sdhaetia@naver.com>
2024-05-29 13:09:16 +0900

    Initial Commit

---
+++ PracticeApp/PracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeAppApp.swift
+//  PracticeApp
+//
+//  Created by SEONGSOO PARK on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
